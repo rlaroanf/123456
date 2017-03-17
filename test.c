c090f4430c996ee349f8ef3691d1fd8aa5beddf9
rlaroanf <k901018@naver.com>
2017-03-17 10:22:07 +0900

    7

--- test.c
+++ test.c
@@ -5,9 +5,16 @@ int main(){
    int i,j;
 
    
-   for(int i=0;i<10;i++)
+   for(i=0;i<10;i++)
       printf("*\n");
 
+
+
+   for(i=0;i<10;i++){
+	   for(j=0;j<10;j++)
+		   printf("*");
+	   printf("\n");
+   }
 
 
    return 0;
