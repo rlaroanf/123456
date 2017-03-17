b77fdf5d9c5f8f9c565a973b95b8647161f43b33
rlaroanf <k901018@naver.com>
2017-03-17 10:27:16 +0900

    9

--- test.c
+++ test.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 
+
+int call(int n){
+	if(n==0) return 0;
+	return n+call(n-1);
+
+}
+
+
 int main(){
    
-   int i,j;
+	int i,j;
 
    
-   for(i=0;i<10;i++)
-      printf("*\n");
+   	for(i=0;i<10;i++)
+      	printf("*\n");
 
 
 
-   for(i=0;i<10;i++){
-	   for(j=0;j<10;j++)
-		   printf("*");
-	   printf("\n");
-   }
+   	for(i=0;i<10;i++){
+		for(j=0;j<10;j++)
+			printf("*");
+	   	printf("\n");
+   	}
 
 
 
-   printf("hello world");
+   	printf("hello world");
 
 
-   return 0;
+	printf("%d",call(5));
+
+
+
+   	return 0;
 }
